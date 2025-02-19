8abdea54b0e7c36e745e74c07136809b2722e3e9
JC666 <jessie.pc.chen@viewsonic.com>
2025-02-19 09:21:37 +0800

    1.優化ListNode寫法(不使用反引號保留字作法)

--- src/utils/ListNode.kt
+++ src/utils/ListNode.kt
@@ -1,24 +1,19 @@
 package utils
 
-class ListNode constructor(var value: Int = -1, var next: ListNode? = null) {
+
+class ListNode(var value: Int = -1, var next: ListNode? = null) {
     override fun toString(): String {
         return "$value -> ${next.toString()}"
     }
 
     companion object {
-        // a help function to generate a linked list with given values quickly, for test purpose only
+        // 快速生成鏈結串列，用於測試
         fun quickList(nodes: List<Int>): ListNode? {
             val dummy = ListNode()
-            nodes.reversed().forEach({
-                val temp = ListNode(it)
-                temp.next = dummy.next
-                dummy.next = temp
-            })
-            if(dummy.next != null) {
-                return dummy.next!!
-            } else {
-                return null
+            nodes.reversed().forEach {
+                dummy.next = ListNode(it, dummy.next)
             }
+            return dummy.next
         }
     }
 }
